6fc7cfd6ce05a831613a393363dace2f5dc2d112
SAIDMUROD <Saidmurodjumanazarov1@gmail.com>
2022-06-10 10:13:19 +0500

    version-1

---
+++ JavaScript/script.js
@@ -0,0 +1,12 @@
+let parent = document.querySelector(".parent");
+let parentChild = document.querySelector(".parent div");
+let btns = document.querySelectorAll(".btn");
+let image = document.querySelector("img");
+
+
+let on = function (){
+    image.src = "./IMG/lampochka_yoniq.gif" 
+}
+let off = function (){
+    image.src = "./IMG/lampochka_ochik.gif" 
+}
